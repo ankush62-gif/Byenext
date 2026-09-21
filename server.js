ae186bda9ca6e8abb9db81449e3e9de2127e8ee3
ankush62-gif <ankush7856883212@gmail.com>
2026-09-21 17:24:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ let data = {
     {
       id: 1,
       name: "Admin",
-      email: email: "ankushpatel9605@gmail.com",
+      email: email: email: "admin@byenext.com",
       password: "Ankush@2009",
       role: "admin"
     }
